34f0e8f82b5a9c9ed2bce6aac375d1346ec55042
Dobbymin <gmin990624@gmail.com>
2026-03-23 01:04:28 +0900

    feat: useSyncExternalStore를 활용한 hydration 처리 구현

---
+++ src/features/main/_hooks/useIsHydrated.ts
@@ -0,0 +1,11 @@
+import { useSyncExternalStore } from "react";
+
+const subscribe = () => () => {};
+
+export const useIsHydrated = () => {
+  return useSyncExternalStore(
+    subscribe,
+    () => true, // 클라이언트
+    () => false, // 서버
+  );
+};
