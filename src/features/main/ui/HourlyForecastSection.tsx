0a2b732c138e7ec1707741f4443ff724a554e853
Dobbymin <gmin990624@gmail.com>
2026-03-22 17:22:59 +0900

    feat: 시간별 예보 전체보기 및 접기 기능 구현

--- src/features/main/ui/HourlyForecastSection.tsx
+++ src/features/main/ui/HourlyForecastSection.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
 import { HourlyForecastList, HourlyForecastTitle } from "../_components";
 
 export const HourlyForecastSection = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <section aria-labelledby='hourly-forecast'>
-      <HourlyForecastTitle />
-      <HourlyForecastList />
+      <HourlyForecastTitle isExpanded={isExpanded} onToggle={() => setIsExpanded((prev) => !prev)} />
+      <HourlyForecastList isExpanded={isExpanded} />
     </section>
   );
 };
