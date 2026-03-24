97fb91da08648a4f6a05accf70a3691cb9367a64
Dobbymin <gmin990624@gmail.com>
2026-03-25 01:03:59 +0900

    refactor: 상세보기 페이지 header 수정

--- src/widgets/header/ui/DetailHeader.tsx
+++ src/widgets/header/ui/DetailHeader.tsx
@@ -1,18 +1,18 @@
+"use client";
+
 import Link from "next/link";
 
-import { useWeatherDetail } from "@/entities";
-import { Button, ROUTE_PATH, Skeleton } from "@/shared";
+import { Button, ROUTE_PATH } from "@/shared";
 import { ChevronLeft } from "lucide-react";
 
-import { HeaderActionButtons, LocationSearch } from "../components";
+import { HeaderActionButtons } from "../components";
 
 type Props = {
   locationId: string;
+  locationName: string;
 };
 
-export const DetailHeader = ({ locationId }: Props) => {
-  const { data, isLoading } = useWeatherDetail(locationId);
-
+export const DetailHeader = ({ locationId, locationName }: Props) => {
   return (
     <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:px-6'>
       <div className='flex items-center gap-2 md:gap-4'>
@@ -27,19 +27,12 @@ export const DetailHeader = ({ locationId }: Props) => {
             <ChevronLeft size={20} className='text-foreground' />
           </Link>
         </Button>
-        <div className='flex flex-col gap-0.5 md:gap-1'>
-          <h1 className='line-clamp-1 font-display text-base font-bold tracking-tight text-foreground md:text-2xl'>
-            {isLoading || !data ? <Skeleton className='h-6 w-32 md:h-8 md:w-64' /> : data.locationName}
-          </h1>
-          <p className='text-[8px] font-semibold tracking-[1px] text-muted-foreground uppercase md:text-[10px]'>
-            Current Location
-          </p>
-        </div>
+        <h1 className='line-clamp-1 font-display text-base font-bold tracking-tight text-foreground md:text-2xl'>
+          {locationName}
+        </h1>
       </div>
-      <div className='flex items-center gap-2 md:gap-4'>
-        <LocationSearch />
-        <HeaderActionButtons locationId={locationId} locationName={data?.locationName} />
-      </div>
+      <HeaderActionButtons locationId={locationId} locationName={locationName} />
     </div>
   );
 };
+
