fe2b36f22cbcf6f68fb8a2a2db2e607878ab3299
Dobbymin <gmin990624@gmail.com>
2026-03-25 01:03:20 +0900

    chore: 1시간 간격의 날씨 데이터 api 주소 및 키 추가

--- src/shared/constants/base-url.ts
+++ src/shared/constants/base-url.ts
@@ -1,3 +1,5 @@
 export const WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_PRO_API_BASE_URL = "https://pro.openweathermap.org/data/2.5";
 
 export const WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
+export const WEATHER_PRO_API_KEY = process.env.OPEN_WEATHER_PRO_API_KEY;
